3e3d9f70633781537f0815558c17d51c8e409922
Thiago Moraes <thiago4x70@gmail.com>
2019-01-30 15:38:13 -0200

    Implement the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-angular-app';
+  todoArray = [];
+
+  addTodo(value){
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  deleteItem(todo) {
+    for(let i = 0; i < this.todoArray.length; i++) {
+      if(todo === this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+
+  todoSubmit(value: any) {
+    if(value !== "") {
+      this.todoArray.push(value.todo);
+      //this.todoForm.reset();
+    } else {
+      alert('Field required!');
+    }
+  }
+
 }
